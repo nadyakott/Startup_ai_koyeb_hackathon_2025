3db06c8472695fb93ec11efa7867fd451c010075
Nadezhda Kotelnikova <n.kotelnikova@cs.money>
2025-02-16 16:32:52 +0300

    ai agents

--- source/ai_agents/linkedin_company.py
+++ source/ai_agents/linkedin_company.py
@@ -1,9 +1,18 @@
-from agent_base import agent
+from agent_base import agent, linkedin
+from source.api.linkedin import LinkedInAPI
+from json_output_format import COMPANY_OUTPUT_JSON_FORMAT
 
+linkedin = LinkedInAPI()
 
-
-def extract_data(founder_data):
+def fetch_company(company_link):
     responses = {}
-    for question in questions:
-            response = agent.run(f"Based on this data {founder_data}, answer the question {question}")
-    return responses
+    output = linkedin.get_company_info(company_link)
+
+    # response = agent.run(f"Based on this data {output}, sum up information about company. Use as an output the following json format {COMPANY_OUTPUT_JSON_FORMAT}")
+    response = agent.run(f"Based on this data {output}, sum up information about company.")
+    return response
+
+if __name__ == '__main__':
+    company_link = 'https://www.linkedin.com/company/ex-corp/'
+    res = fetch_company(company_link)
+    print(res)
